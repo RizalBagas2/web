8ccbb5354a9ef6f259d896160d81c3e1c73ed387
RizalBagas2 <rizalbagastristanto4@gmail.com>
2024-11-21 14:47:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js - Tambahkan interaksi di sini
+document.querySelectorAll('button').forEach(function(button) {
+    button.addEventListener('click', function() {
+        alert('Terima kasih sudah memesan kopi! Nikmati secangkir kebahagiaan dari Starbucks.');
+    });
+});
